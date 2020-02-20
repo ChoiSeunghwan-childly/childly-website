586f775287ed7b68e1eb5ed79b7a125e7a25100d
ChoiSeunghwan-childly <csh@childly.com>
2020-02-20 14:02:55 +0900

    push

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,3 +2,8 @@ function dovewalletMoreInfoToggle() {
   var element = document.getElementById('service-dovewallet-more-info');
   element.classList.toggle("show");
 }
+
+function minigpoolhubMoreInfoToggle() {
+  var element = document.getElementById('service-minigpoolhub-more-info');
+  element.classList.toggle("show");
+}
